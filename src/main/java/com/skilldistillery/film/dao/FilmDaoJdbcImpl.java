ca8f8db9c3dd5df30d0c06dbe6de5f47ede06fad
Sawyer Harnedy <sawyer@Brucebs-MacBook-Pro-2.local>
2023-09-08 17:18:56 -0600

    config spring smoke test

---
+++ src/main/java/com/skilldistillery/film/dao/FilmDaoJdbcImpl.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.film.dao;
+
+public class FilmDaoJdbcImpl implements FilmDAO {
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=US/Mountain";
+	private static final String USER = "student";
+	private static final String PASS = "student";
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Error loading MySQL Driver");
+			throw new RuntimeException("Unable to load MySQL Driver class");
+		}
+	}
+
+}
