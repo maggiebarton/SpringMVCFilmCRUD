521e5b2dbd98ea4faf5d97b96db8aede556e07f1
Sawyer Harnedy <sawyer@Brucebs-MacBook-Pro-2.local>
2023-09-08 18:12:02 -0600

    adding JSP, modified controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,6 +2,10 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.film.dao.FilmDAO;
 
@@ -10,5 +14,10 @@ public class FilmController {
 	@Autowired
 	private FilmDAO dao;
 	
-	
+	@RequestMapping(path="getFilm.do", method=RequestMethod.GET)
+	public String getFilm(@RequestParam Integer filmId, Model model) {
+		System.out.println("filmID:" + filmId);
+		model.addAttribute("filmId", filmId);
+		return "WEB-INF/film.jsp";
+	}
 }
